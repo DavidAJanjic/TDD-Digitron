6b08f4d4b7f3d610391cf1880da0f9706bf1a93c
David <davidajanjic1997@gmail.com>
2020-03-24 16:12:00 +0100

    Eliminated while loop in Parser Eliminated Scanner x in Parser

--- src/main/java/com/company/Parser.java
+++ src/main/java/com/company/Parser.java
@@ -6,48 +6,46 @@ import java.util.Scanner;
 public class Parser {
 
     public static void parse(String s) {
-        Scanner x = new Scanner(s);
         ArrayList<Double> numbers = new ArrayList<>();
         ArrayList<Operations> operations = new ArrayList<>();
-        while (x.hasNext()) {
-            String d = "";
-            String c = x.next();
-            for (int n = 0; n < c.length(); n++) {
-                char b = c.charAt(n);
-                String a = Character.toString(b);
-                if (isNumeric(a)) {
-                    d += a;
-                    continue;
-                }
-                if (d != "") {
-                    numbers.add(Double.parseDouble(d));
-                    d = "";
-                }
-                if (a.equals("+")) {
-                    operations.add(new Addition());
-                    continue;
-                }
-                if (a.equals("-")) {
-                    operations.add(new Subtraction());
-                    continue;
-                }
-                if (a.equals("*")) {
-                    operations.add(new Multiplication());
-                    continue;
-                }
-                if (a.equals("/")) {
-                    operations.add(new Division());
-                }
+
+        String d = "";
+
+        for (int n = 0; n < s.length(); n++) {
+            char b = s.charAt(n);
+            String a = Character.toString(b);
+            if (isNumeric(a)) {
+                d += a;
+                continue;
             }
             if (d != "") {
                 numbers.add(Double.parseDouble(d));
                 d = "";
             }
-
+            if (a.equals("+")) {
+                operations.add(new Addition());
+                continue;
+            }
+            if (a.equals("-")) {
+                operations.add(new Subtraction());
+                continue;
+            }
+            if (a.equals("*")) {
+                operations.add(new Multiplication());
+                continue;
+            }
+            if (a.equals("/")) {
+                operations.add(new Division());
+            }
         }
+        if (d != "") {
+            numbers.add(Double.parseDouble(d));
+            d = "";
+        }
+
         Arrays.setNumbers(numbers);
         Arrays.setOperations(operations);
-        x.close();
+
     }
 
     private static boolean isNumeric(String s) {
--- src/main/java/com/company/TestParser.java
+++ src/main/java/com/company/TestParser.java
@@ -10,12 +10,12 @@ public class TestParser {
     @Test
     public void test1() {
         Parser.parse("1 +2* 3-4/ 2");
-
+        assertEquals(5, Calculator.calculate());
     }
 
     @Test
     public void test2() {
-        Parser.parse("50 - 2* 10 + 60/3 + 4 * 5 ");
+        Parser.parse("50 - 2* 10 + 60/3 + 4 * 5");
         assertEquals(70, Calculator.calculate());
     }
 }
